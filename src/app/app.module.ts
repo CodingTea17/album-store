dd447ca954d1281073aa411080b0db95c9ecdc10
DanDanilyuk <DanDanilyuk@gmail.com>
2017-10-29 21:17:28 -0700

    finish guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+   BrowserModule,
+   FormsModule,
+   HttpModule,
+   routing
   ],
   providers: [],
   bootstrap: [AppComponent]
